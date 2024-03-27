016934651a3c092ab5aa431dadee608364f9e936
Никита Жинов <zhinovnikita@yandex.ru>
2024-03-27 12:19:12 +0700

    lib8

--- main.cpp
+++ main.cpp
@@ -36,9 +36,20 @@ int main() {
     spravo4nik.push_back({ "Абвгдов", "Абвгд", "Абвгдеев", 89000000000 });
     spravo4nik.push_back({ "Охлобыстин", "Иван", "Иванович", 0 });
 
-    ShellSort(spravo4nik, commpare);
+    std::vector<size_t> B(spravo4nik.size());
+    FillInc(B, B.size());
 
-    for (int i = 0; i < spravo4nik.size(); i++)
+    size_t M = (int)std::log2(spravo4nik.size()) - 1;
+    std::vector<int> h(M);
+    h[0] = 1;
+    for (int i = 1; i < M; i++)
+        h[i] = 2 * h[i - 1] + 1;
+    for (int a = M - 1; a >= 0; a--)
+        for (size_t i = h[a]; i < spravo4nik.size(); i++)
+            for (int j = i - h[a]; j >= 0 and commpare(spravo4nik[B[j]], spravo4nik[B[j + h[a]]]); j -= h[a])
+                std::swap(B[j], B[j + h[a]]);
+
+    for (auto i : B)
         std::println("{:11} {:7} {:15} {:11}", spravo4nik[i].surname, spravo4nik[i].name, spravo4nik[i].patronymic, spravo4nik[i].number);
 
     return 0;
